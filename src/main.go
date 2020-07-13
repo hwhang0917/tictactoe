8aa0eff475aeba397f8d9920d07879f97f1eda59
Mark Whang <hwhang0917@gmail.com>
2020-07-13 23:54:50 +0900

    Add minimax algorithm (need fix)

--- src/main.go
+++ src/main.go
@@ -6,11 +6,25 @@ import (
 	"github.com/tictactoe/src/ttt"
 )
 
+// O is the integer value for O player
+const O int = 1
+
+// X is the integer value for X player
+const X int = 2
+
+// E is the integer value for empty space
+const E int = 0
+
 func main() {
 	sampleState := [9]int{
-		2, 2, 1,
-		0, 1, 1,
-		2, 0, 1,
+		E, X, O,
+		E, O, E,
+		E, E, E,
 	}
-	fmt.Println(ttt.EvaluateGame(sampleState))
+
+	// fmt.Println(ttt.EvaluateGame(sampleState))
+	// fmt.Println(ttt.IsFinished(sampleState))
+
+	// Expected 6
+	fmt.Println(ttt.GetNextAIMove(sampleState))
 }
--- src/ttt/ttt.go
+++ src/ttt/ttt.go
@@ -1,7 +1,21 @@
 package ttt
 
-// EvaluateGame evaluates the game with given state (-1: Error, 0: Game continues, 1: Player 1 wins, 2: Player 2 wins)
+import (
+	"log"
+)
+
+// O is the integer value for O player
+const O int = 1
+
+// X is the integer value for X player
+const X int = 2
+
+// EMPTY is the integer value for empty space
+const EMPTY int = 0
+
+// EvaluateGame evaluates the game with given state (1: Max player wins, -1: Min player wins, 0: Draw or Game is still on)
 func EvaluateGame(state [9]int) int {
+	// Winning cases' indexes
 	winningCases := [8][3]int{
 		{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // Row
 		{0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // Column
@@ -10,35 +24,160 @@ func EvaluateGame(state [9]int) int {
 
 	// O,X Count
 	var (
-		p1Count int
-		p2Count int
+		maxCount int
+		minCount int
 	)
 
 	for _, winningCase := range winningCases {
-		p1Count = 0
-		p2Count = 0
+		maxCount = 0
+		minCount = 0
 		for _, winIdx := range winningCase {
 			switch state[winIdx] {
-			case 1:
-				p1Count++
+			case X:
+				maxCount++
 				break
-			case 2:
-				p2Count++
+			case O:
+				minCount++
 				break
-			case 0:
+			case EMPTY:
 				break
 			default: // Error with state
-				return -1
+				log.Fatal("[ERROR] There is something wrong with the state. Values should be 0,1,or 2.")
 			}
 		}
 
 		// Check for winner
-		if p1Count == 3 { // Player 1 wins
+		if maxCount == 3 { // Player 1 wins
 			return 1
-		} else if p2Count == 3 { // Player 2 wins
-			return 2
+		} else if minCount == 3 { // Player 2 wins
+			return -1
 		}
 	}
-	// Game is still on
+	// Game is still on || Game is a Draw
 	return 0
+}
+
+// IsFinished returns 1 if the game is finished, 0 if is still on, -1 if error
+func IsFinished(state [9]int) bool {
+	emptyCell := 9
+
+	for _, box := range state {
+		if box == O || box == X {
+			emptyCell--
+		} else if box == EMPTY {
+			// Do nothing
+		} else { // Error state
+			log.Fatal("[ERROR] There is something wrong with the state. Values should be 0,1,or 2.")
+		}
+	}
+
+	if emptyCell == 0 { // Game is finished
+		return true
+	}
+	// Game is still on
+	return false
+}
+
+// GetNextAIMove gets the next best AI move based on Minimax algorithm
+func GetNextAIMove(state [9]int) int {
+	action := minimax(state, true)
+
+	return action
+}
+
+func minimax(state [9]int, maxTurn bool) int {
+	action := 0
+	if maxTurn { // Max Player's Turn
+		maxv := -2000
+		// action = 0
+		for i := 0; i < 9; i++ {
+			if state[i] == EMPTY { // a valid move
+				newState := state
+				newState[i] = X
+				val := minValue(newState, -2000, 2000)
+				if val > maxv {
+					maxv = val
+					action = i
+				}
+			}
+		}
+	} else { // Min Player's Turn
+		minv := 2000
+		// action = 0
+		for i := 0; i < 9; i++ {
+			if state[i] == EMPTY { // a valid move
+				newState := state
+				newState[i] = O
+				val := maxValue(newState, -2000, 2000)
+
+				if val < minv {
+					minv = val
+					action = i
+				}
+			}
+		}
+	}
+	return action
+}
+
+func minValue(state [9]int, alpha int, beta int) int {
+	// Terminal Test
+	evaluation := EvaluateGame(state)
+	if evaluation == 1 || evaluation == -1 { // Winning condition
+		return EvaluateGame(state) // Returns 1 or -1
+	} else if IsFinished(state) { // Draw
+		return EvaluateGame(state)
+	}
+
+	utilityValue := 2000 // Arbitrary value
+	for i := 0; i < 9; i++ {
+		if state[i] == EMPTY {
+			newState := state
+			newState[i] = O
+
+			tmpValue := maxValue(newState, alpha, beta)
+
+			if tmpValue < utilityValue {
+				utilityValue = tmpValue
+			}
+			if utilityValue <= alpha {
+				return utilityValue
+			}
+			if beta > utilityValue {
+				beta = utilityValue
+			}
+		}
+	}
+	return utilityValue
+}
+
+func maxValue(state [9]int, alpha int, beta int) int {
+	// Terminal Test
+	evaluation := EvaluateGame(state)
+	if evaluation == 1 || evaluation == -1 { // Winning condition
+		return EvaluateGame(state) // Returns 1 or -1
+	} else if IsFinished(state) { // Draw
+		return EvaluateGame(state)
+	}
+
+	utilityValue := -2000 // Arbitrary value
+	for i := 0; i < 9; i++ {
+		if state[i] == EMPTY {
+			newState := state
+			newState[i] = X
+
+			tmpValue := minValue(newState, alpha, beta)
+
+			if tmpValue > utilityValue {
+				utilityValue = tmpValue
+			}
+			if utilityValue >= beta {
+				return utilityValue
+			}
+			if alpha < utilityValue {
+				alpha = utilityValue
+			}
+		}
+	}
+	return utilityValue
 }
